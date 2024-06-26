fd7107714f5e21b726574dae77e1376f80c0d2a2
Guim Casadellà <guim.casadella@gmail.com>
2024-06-26 19:24:20 +0200

    Lora script cleaned up

--- scripts/lora_gta_to_cityscapes_clean.py
+++ scripts/lora_gta_to_cityscapes_clean.py
@@ -1,23 +1,33 @@
-# Load the dataset
-
+# Datasets
 from datasets import load_from_disk
-import matplotlib.pyplot as plt
+# Model
+from transformers import SegformerForSemanticSegmentation, SegformerImageProcessor
+# Preprocessing
+import json
+from huggingface_hub import cached_download, hf_hub_url
+from torchvision.transforms import ColorJitter
+from PIL import Image
+import numpy as np
+# Metrics
+import torch
+from torch import nn
+import evaluate
+# LoRA
+from peft import LoraConfig, get_peft_model
+# Train
+from transformers import TrainingArguments, Trainer
 
+
+##################### DATASET #####################
 # Load the dataset from disk
 loaded_dataset = load_from_disk("../cityscapes_train_1000_dataset_v3")
-print("Dataset loaded successfully.")
 
 # Prepare train and test splits
-
 loaded_dataset = loaded_dataset.train_test_split(test_size=0.1)
 train_ds = loaded_dataset["train"]
 test_ds = loaded_dataset["test"]
 
-# Label mappers
-
-import json
-from huggingface_hub import cached_download, hf_hub_url
-
+##################### IDs and LABELs #####################
 repo_id = "huggingface/label-files"
 filename = "cityscapes-id2label.json"
 id2label = json.load(open(cached_download(hf_hub_url(repo_id, filename, repo_type="dataset")), "r"))
@@ -25,25 +35,19 @@ id2label = {int(k): v for k, v in id2label.items()}
 label2id = {v: k for k, v in id2label.items()}
 num_labels = len(id2label)
 
-# Model
-
-from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation, SegformerImageProcessor
-
-checkpoint_dir = "guimCC/segformer-v0-gta"
+##################### BASE MODEL #####################
+# id of the model to load, SegFormer pretrained on gta 
+model_id = "guimCC/segformer-v0-gta"
 
 image_processor = SegformerImageProcessor()
 
-model = SegformerForSemanticSegmentation.from_pretrained(checkpoint_dir)
+model = SegformerForSemanticSegmentation.from_pretrained(model_id)
 
 
-# Preprocessing
-from torchvision.transforms import ColorJitter
+##################### TRANSFORMATIONS #####################
 
 # Transofrms the color properities
 jitter = ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.1)
-from PIL import Image
-import numpy as np
-
 
 def handle_grayscale_image(image):
     np_image = np.array(image)
@@ -52,7 +56,6 @@ def handle_grayscale_image(image):
         return Image.fromarray(tiled_image)
     else:
         return Image.fromarray(np_image)
-
 
 def train_transforms(example_batch):
     images = [jitter(handle_grayscale_image(x)) for x in example_batch["image"]]
@@ -70,13 +73,9 @@ def val_transforms(example_batch):
 train_ds.set_transform(train_transforms)
 test_ds.set_transform(val_transforms)
 
-# Metric
-import torch
-from torch import nn
-import evaluate
+##################### METRICS #####################
 
 metric = evaluate.load("mean_iou")
-
 
 def compute_metrics(eval_pred):
     with torch.no_grad(): # Don't want to store the gradients while computing this metric since it's validation
@@ -100,7 +99,6 @@ def compute_metrics(eval_pred):
             ignore_index=0,
             reduce_labels=image_processor.do_reduce_labels,
         )
-
         # add per category metrics as individual key-value pairs
         per_category_accuracy = metrics.pop("per_category_accuracy").tolist()
         per_category_iou = metrics.pop("per_category_iou").tolist()
@@ -110,8 +108,7 @@ def compute_metrics(eval_pred):
 
         return metrics
     
-# Wrap the model:
-from peft import LoraConfig, get_peft_model
+##################### LORA MODEL #####################
 
 config = LoraConfig(
     r=32,
@@ -122,13 +119,10 @@ config = LoraConfig(
     modules_to_save=["decode_head"],
 )
 lora_model = get_peft_model(model, config)
-#print_trainable_parameters(lora_model)
 
+##################### TRAINING #####################
 
-# train
-from transformers import AutoModelForSemanticSegmentation, TrainingArguments, Trainer
-
-model_name = checkpoint_dir.split("/")[-1]
+model_name = model_id.split("/")[-1]
 
 training_args = TrainingArguments(
     output_dir=f"LORA_test/{model_name}-cityscapes",
